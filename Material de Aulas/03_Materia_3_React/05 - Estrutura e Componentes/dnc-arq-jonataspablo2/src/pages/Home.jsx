d0e97e153ec752d7173c65a3410ebd57c69edae3
JonatasPablo <jonathaspabllo@gmail.com>
2025-03-06 12:08:08 -0300

    Aulas

---
+++ Material de Aulas/03_Materia_3_React/05 - Estrutura e Componentes/dnc-arq-jonataspablo2/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+
+function Home(){
+    return(
+        <>
+            <Header/>
+            <h1>Home</h1>
+            <p>Welcome to the main page</p>
+        </>
+    )
+}
+
+export default Home;
