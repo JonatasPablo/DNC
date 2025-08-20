6ebcde2cee0d8e2879a75c310ca992b7ea656d16
JonatasPablo <jonathaspabllo@gmail.com>
2025-08-19 23:54:12 -0300

    Aulas

--- Material_de_Aulas/06_React/CODE/dnc-arq-jonatas/src/components/ProjectsList/ProjectsList.jsx
+++ Material_de_Aulas/06_React/CODE/dnc-arq-jonatas/src/components/ProjectsList/ProjectsList.jsx
@@ -1,11 +1,29 @@
-import './ProjectsList.css'
+import { useState, useEffect } from 'react';
+import './ProjectsList.css';
 
-//ASSETS
-import LikedFilled from '../../assets/like-filled.svg'
-import Like from '../../assets/like.svg'
+// ASSETS
+import LikedFilled from '../../assets/like-filled.svg';
+import Like from '../../assets/like.svg';
 
+// UTILS
+import { getApiData } from '../../services/apiServices';
 
 function ProjectsList() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const projectsResponse = await getApiData('projects');
+                setProjects(projectsResponse);
+            } catch (error) {
+                console.error("Erro ao carregar projetos: ", error);
+                setProjects([]);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className="projects-section">
             <div className="projects-hero">
@@ -13,58 +31,20 @@ function ProjectsList() {
                 <p>It is a long established fact that a reader will be distracted by the of readable content of page lookings at its layouts points.</p>
             </div>
             <div className="projects-grid">
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" />
-                </div>
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={Like} height="20px" />
-                </div>
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" />
-                </div>
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={Like} height="20px" />
-                </div>
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" />
-                </div>
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={Like} height="20px" />
-                </div>
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" />
-                </div>
-                <div className="project-card d-flex jc-center al-center fd-column">
-                    <div className="thumb tertiary-background"></div>
-                    <h3>João Silva</h3>
-                    <p>BH, Brasil</p>
-                    <img src={LikedFilled} height="20px" />
-                </div>
+                {Array.isArray(projects) && projects.map((project) => (
+                    <div className="project-card d-flex jc-center al-center fd-column" key={project.id}>
+                        <div 
+                            className="thumb tertiary-background"
+                            style={{ backgroundImage: `url(${project.thumb})` }}>
+                        </div>
+                        <h3>{project.title}</h3>
+                        <p>{project.subtitle}</p>
+                        <img src={LikedFilled} height="20px" />
+                    </div>
+                ))}
             </div>
-            
         </div>
-    )
+    );
 }
 
-export default ProjectsList
+export default ProjectsList;
