970c25f101f07477d58a5df7a362bf7990f63220
JonatasPablo <jonathaspabllo@gmail.com>
2025-09-03 00:01:52 -0300

    Aulas

---
+++ Material_de_Aulas/07_React_com_Typescript/introducao-typescript/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const paciente = {
+    nome: 'Jonatas Pablo',
+    idade: '30'
+};
+console.log(paciente);
+//# sourceMappingURL=index.js.map
---
+++ Material_de_Aulas/07_React_com_Typescript/introducao-typescript/src/index.ts
@@ -0,0 +1,9 @@
+const paciente: {
+    nome: string,
+    idade: string
+} = {
+    nome: 'Jonatas Pablo',
+    idade: '30'
+}
+
+console.log(paciente)
